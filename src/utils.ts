7b5e50cf51232c1f6f833268e21dcd920fe6d141
ModyQyW <wurui-dev@foxmail.com>
2023-04-23 15:00:41 +0800

    feat: add theme colors default value

--- src/utils.ts
+++ src/utils.ts
@@ -2,4 +2,10 @@ import { generate as antdGenerate } from '@ant-design/colors'
 
 export const convertArrayToObject = (array: string[]) => Object.fromEntries(array.map((item, index) => [index + 1, item]))
 
-export const generate = (color: string, theme: 'default' | 'dark', backgroundColor: string) => convertArrayToObject(antdGenerate(color, { theme, backgroundColor }))
+export function generate(color: string, theme: 'default' | 'dark', backgroundColor: string) {
+  const array = antdGenerate(color, { theme, backgroundColor })
+  return {
+    ...convertArrayToObject(array),
+    DEFAULT: array[5],
+  }
+}
